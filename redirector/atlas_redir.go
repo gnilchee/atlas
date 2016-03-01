91f6df7adf53ff9e330dd56422a807f875ae850b
Greg Nilchee <gnilchee@Gregs-MBP.hsd1.co.comcast.net>
2016-02-29 18:14:05 -0700

    added redirector and https

---
+++ redirector/atlas_redir.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+    "net/http"
+)
+
+func redir(w http.ResponseWriter, req *http.Request) {
+	http.Redirect(w, req, "https://hostname.com"+req.RequestURI, http.StatusMovedPermanently)
+}
+
+func main() {
+	http.ListenAndServe("0.0.0.0:8080", http.HandlerFunc(redir))
+}
