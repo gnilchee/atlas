fd04709206fc6c04ba744ea2584d2ccb1c581f28
Greg Nilchee <gnilchee@Gregs-MBP.hsd1.co.comcast.net>
2016-03-02 22:25:34 -0700

    merged redirector and https into single go file and update compose file

---
+++ server/atlas.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+    "log"
+    "net/http"
+)
+
+func main() {
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/", fs)
+
+    log.Println("Server is listening on 4443 and 8080...")
+    http.ListenAndServeTLS("0.0.0.0:4443", "/etc/atlas/ssl/certfile.crt", "/etc/atlas/ssl/keyfile.key", nil)
+    http.ListenAndServe("0.0.0.0:8080", http.HandlerFunc(redirector))
+}
+
+func redirector(w http.ResponseWriter, req *http.Request) {
+	http.Redirect(w, req, "https://hostname.com"+req.RequestURI, http.StatusMovedPermanently)
+}
